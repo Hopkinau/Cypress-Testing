8c31a0d58671b2236c22e84bd7ce890503544ca2
Hopkin HE <hopkinau@gmail.com>
2023-08-19 22:01:10 +1000

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/integration/examples/*.js',
+    //Cypress Runner will check the specPattern files and run the tests
+  },
+});
---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,7 @@
+//Cypress-Spec for Test1
+describe('My First Test Suite', function () {
+  it('My First Test Case', function () {
+    //First Steps visit the URL
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+  });
+});
