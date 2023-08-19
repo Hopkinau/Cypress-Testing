eebdac3ad49af24caee1cc38abfadc5e73c5bea2
PENG HE <hopkinau@gmail.com>
2023-08-19 22:04:22 +1000

    update change

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+//Cypress-Spec for Test1
+describe('My First Test Suite', function () {
+  it('My First Test Case', function () {
+    //First Steps visit the URL
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+  });
+});
+=======
+//Cypress-Spec for Test1
+describe('My First Test Suite', function () {
+  it('My First Test Case', function () {
+    //First Steps visit the URL
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+  });
+});
+>>>>>>> 8c31a0d58671b2236c22e84bd7ce890503544ca2
