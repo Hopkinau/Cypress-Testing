afd4871144b29ff321ceb2f66a4b4bf59b9d3541
PENG HE <hopkinau@gmail.com>
2023-08-19 23:35:07 +1000

    Test steps

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,15 +1,13 @@
 //Cypress-Spec for Test1
+
 describe('My First Test Suite', function () {
   it('My First Test Case', function () {
     //First Steps visit the URL
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.get('.search-keyword').type('ca');
+    cy.wait(2000);
+    cy.get('.product:visible').should('have.length', 4);
   });
 });
 
 //Cypress-Spec for Test1
-describe('My First Test Suite', function () {
-  it('My First Test Case', function () {
-    //First Steps visit the URL
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-  });
-});
