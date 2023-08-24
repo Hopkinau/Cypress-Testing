735d522359bdccae122a1ddd1250fb8c373a64df
PENG HE <hopkinau@gmail.com>
2023-08-24 14:00:21 +1000

    JQuery text () method

--- cypress/integration/examples/hooks.js
+++ cypress/integration/examples/hooks.js
@@ -36,9 +36,16 @@ describe('Hooks Suite', function () {
     cy.contains('Checkout').click();
     cy.get('#country').type('India');
     cy.get('.suggestions > ul > li > a').click();
-    // cy.get('#checkbox2').click({ force: true });
-
-    //loop example.json data and use custom command to test the product
+    cy.get('#checkbox2').click({ force: true });
+    cy.get("input[type='submit']").click();
+    //cy.get('.alert').should(
+    //   'have.text',
+    //   'Success! Thank you! Your order will be delivered in next few weeks :-).'
+    // );
+    cy.get('.alert').then(function (element) {
+      const text = element.text();
+      expect(text.includes('Success')).to.be.true;
+    });
 
     // beforeEach(() => {
     //   // root-level hook
